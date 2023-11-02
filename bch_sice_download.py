2e8992faf0a18f182ac66075b004754852c47501
gmargoth <84695064+gmargoth@users.noreply.github.com>
2023-11-01 21:59:19 -0600

    Update bch_sice_download.py

--- bch_sice_download.py
+++ bch_sice_download.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-import requests
-
-def login_SICE(username, password):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = '/path/to/chrome'
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-
-    driver = webdriver.Chrome(options=chrome_options)
+def login_SICE(username, password, save_to):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_experimental_option("prefs", {"download.default_directory": save_to})
+    driver = webdriver.Chrome(options=options)
     driver.get("https://sisee.bch.hn/SICE/Login.aspx")
 
     username_field = driver.find_element(By.NAME, 'ctl00$ContentPlaceHolder1$UserName')
@@ -19,32 +15,17 @@ def login_SICE(username, password):
     username_field.send_keys(username)
     password_field.send_keys(password)
     submit_button.click()
-
-    response = driver.get('https://sisee.bch.hn/SICE/ConsultaSACAjustado.aspx')
-
     return driver
-    
+
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 def downaload_SICE_query(driver, serie, year, month, country):
-    series_dict = dict(Importaciones = 0, Exportaciones = 1, BalanzaCambiaria = 2)
-
     driver.get('https://sisee.bch.hn/SICE/ConsultaSACAjustado.aspx')
+    
 
+    series_dict = dict(Importaciones = 0, Exportaciones = 1, BalanzaCambiaria = 2)
     select_element = driver.find_element(By.ID,'ContentPlaceHolder1_DDLComercio')
     select = Select(select_element)
     select.select_by_value(str(series_dict[serie]))
-
-    dropdown_arrow = driver.find_element(By.ID, "ContentPlaceHolder1_BtnAnual")
-    dropdown_arrow.click()
-
-    wait = WebDriverWait(driver, 10)
-    year_month_window = wait.until(EC.visibility_of_element_located((By.ID, "ContentPlaceHolder1_SeleccionardeListaAnual_BtnSeleccionarTodo")))
-
-    dropdown_arrow = driver.find_element(By.ID, "ContentPlaceHolder1_SeleccionardeListaAnual_BtnSeleccionarTodo")
-    dropdown_arrow.click()
 
     if year:
         ChkAnual = driver.find_element(By.ID,'ContentPlaceHolder1_ChkAnual')
